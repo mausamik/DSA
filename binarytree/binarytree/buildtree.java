43321670e105d96ff21f4445fd773b08c20c0750
Mausami <mausamikarogal01@gmail.com>
2022-05-20 22:52:38 +0530

    update

--- binarytree/binarytree/buildtree.java
+++ binarytree/binarytree/buildtree.java
@@ -1,14 +1,23 @@
 package binarytree;
 
+
+
 /*
 --> BUILD TREE 
 --> COUNT OF NODES 
 --> SUM OF NODES 
 --> HEIGHT OF NODES 
+--> DIAMETER OF NODES (no of nodes in the longest path) 
+        2 approaches 
+
+
 
 */
 
 public class buildtree {
+
+
+
 
 
 static class Node {
@@ -88,6 +97,65 @@ static class Bintree{
         return finalheight;
     }
 
+// diameter approach  1 with O(n2) time complex 
+    public static int diameter(Node root)
+    {
+        // base case 
+        if(root == null)
+            return 0 ;
+        
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+
+        int diam3 = heightofNodes(root.left) + heightofNodes(root.right) + 1 ;
+
+        int finaldiam = Math.max(diam3, Math.max(diam1, diam2));
+        return finaldiam;
+    }
+
+
+    // Diameter approach 2 with O(n) time compl. 
+    static class TreeInfo
+    {
+        int ht;
+        int diam; 
+
+        TreeInfo(int ht, int diam)
+        {
+            this.ht = ht ;
+            this.diam = diam ; 
+
+        }
+    }
+
+    // fucntion 
+    public static TreeInfo diameter2(Node root)
+    {
+        if(root == null)
+            return new TreeInfo(0, 0);
+        
+        
+        //store left subtree and store right subtree
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+
+        // final height
+        int myheight = Math.max(left.ht, right.ht) + 1;
+
+        // extract my diameters 
+        int d1 = left.diam;
+        int d2 = right.diam;
+        int d3 = left.diam + right.diam + 1 ;
+
+        int mydiam = Math.max(d3, Math.max(d1,d2));
+
+        // we have return type as class  --> create an object 
+        TreeInfo t = new TreeInfo(myheight, mydiam);
+        return t ; 
+
+
+    }
+
 
     public static void main(String[] args)
     {
@@ -103,6 +171,11 @@ static class Bintree{
         System.out.println(sumofNodes(root));
 
         System.out.println(heightofNodes(root));
+
+        System.out.println(diameter(root));
+
+        System.out.println(diameter2(root).diam);  // we have TreeInfo as class, and we are printing one attribute(diam) of this class 
+
 
 
     }
